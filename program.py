9911ca01c802926b6e1627f446a8ed1bfaedeccd
Петрин Дмитрий <dapetrin98@yandex.ru>
2020-02-23 14:59:36 +0300

    Внесены исправления

--- program.py
+++ program.py
@@ -1,5 +1,3 @@
-import time
-import sim
 
 def do_action(client_id, wheels):
     move(client_id, wheels, sleep_time, speed)
@@ -10,7 +8,3 @@ def do_action(client_id, wheels):
     rotate(client_id, wheels, 2*sleep_time, -0.5*speed)
     move(client_id, wheels, sleep_time, speed)
     move(client_id, wheels, sleep_time, -speed)
-    rotate(client_id, wheels, sleep_time, -0.5 * speed)
-    move(client_id, wheels, sleep_time, speed)
-    move(client_id, wheels, sleep_time, -speed)
-    rotate(client_id, wheels, 2 * sleep_time, 0.5 * speed)
