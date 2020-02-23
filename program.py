08b157c0779655e7d26c0b6b3867b2da7a122362
ASUS\Хозяин <annakhl151@gmail.com>
2020-02-23 15:51:55 +0300

    Убраны лишние строки

--- program.py
+++ program.py
@@ -6,5 +6,3 @@ def do_action(client_id, wheels):
     move(client_id, wheels, sleep_time, speed)
     move(client_id, wheels, sleep_time, -speed)
     rotate(client_id, wheels, 2*sleep_time, -0.5*speed)
-    move(client_id, wheels, sleep_time, speed)
-    move(client_id, wheels, sleep_time, -speed)
